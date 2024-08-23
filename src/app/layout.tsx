fdc05c1d1ccd02ede28170719939d55b111a42f2
K5desu <dekinousyonn@gmail.com>
2024-08-23 14:15:58 +0900

    errohandle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,34 +13,6 @@ export const metadata: Metadata = {
   icons: {
     icon: "/Horizon.jpg",
   },
-  openGraph: {
-    title: "Next.js",
-    description: "The React Framework for the Web",
-    url: "https://nextjs.org",
-    siteName: "Next.js",
-    images: [
-      {
-        url: "https://nextjs.org/og.png", // Must be an absolute URL
-        width: 800,
-        height: 600,
-      },
-      {
-        url: "https://nextjs.org/og-alt.png", // Must be an absolute URL
-        width: 1800,
-        height: 1600,
-        alt: "My custom alt",
-      },
-    ],
-    videos: [
-      {
-        url: "https://nextjs.org/video.mp4", // Must be an absolute URL
-        width: 800,
-        height: 600,
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
 };
 
 export default function RootLayout({
